00a6c2d4cf4d379840dc5a532df007f66f95d53a
Lukasz Walencik <walencik.lukasz@gmail.com>
2023-12-02 19:43:37 +0100

    Remove all code from runApp and add there only SplashPage with empty function

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,10 @@
+import 'package:chatify_app/pages/splash_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Chatify',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
-        home: const Placeholder());
-  }
+  runApp(
+    SplashPage(
+      onInitializationComplete: () {},
+    ),
+  );
 }
