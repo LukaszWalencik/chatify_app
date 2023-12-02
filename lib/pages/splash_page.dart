8d4ae08ca4f3e774a9126958a3fe92caf64c23fd
Lukasz Walencik <walencik.lukasz@gmail.com>
2023-12-02 20:09:58 +0100

    Create async function setup with initialize Firebase inside and use it in InitialState

--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 class SplashPage extends StatefulWidget {
@@ -9,6 +10,12 @@ class SplashPage extends StatefulWidget {
 }
 
 class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    setup().then((_) => widget.onInitializationComplete);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -31,4 +38,12 @@ class _SplashPageState extends State<SplashPage> {
       )),
     );
   }
+
+  Future<void> setup() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp();
+    _registerServices();
+  }
+
+  void _registerServices() {}
 }
