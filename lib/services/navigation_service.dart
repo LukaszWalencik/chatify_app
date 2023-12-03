6d89732cbacacbf73188aadf0ec1beb027b0b845
Lukasz Walencik <walencik.lukasz@gmail.com>
2023-12-03 01:14:59 +0100

    Create NavigatoirService to quick reusable navigators

---
+++ lib/services/navigation_service.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class NavigatorService {
+  static GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+
+  void removeAndNavigateToRoute(String route) {
+    navigatorKey.currentState?.popAndPushNamed(route);
+  }
+}
