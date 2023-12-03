69c2f47a51481f021fd2aa639e888df50fa1af12
Lukasz Walencik <walencik.lukasz@gmail.com>
2023-12-03 20:41:59 +0100

    Create MainApp class with returnig MaterialApp when InitializationComplete

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,28 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     SplashPage(
-      onInitializationComplete: () {},
+      key: UniqueKey(),
+      onInitializationComplete: () {
+        runApp(const MainApp());
+      },
     ),
   );
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chatify',
+      theme: ThemeData(
+        scaffoldBackgroundColor: const Color.fromRGBO(36, 35, 49, 1.0),
+        bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+          backgroundColor: Color.fromRGBO(30, 29, 37, 1.0),
+        ),
+      ),
+      // navigatorKey: NavigationService.navigatorKey,
+    );
+  }
 }
