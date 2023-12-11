29e1cebe4de14e191bcfc333a8ee2a05a983be66
Lukasz Walencik <walencik.lukasz@gmail.com>
2023-12-11 16:07:10 +0100

    Create CustomB utton widget

---
+++ lib/widgets/custom_button.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  final String name;
+  final double height;
+  final double width;
+  final Function onPressed;
+  const CustomButton(
+      {super.key,
+      required this.name,
+      required this.height,
+      required this.width,
+      required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: height,
+      width: width,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(height * 0.25),
+        color: Color.fromRGBO(0, 82, 218, 1.0),
+      ),
+      child: TextButton(
+        child: Text(
+          name,
+          style: TextStyle(fontSize: 22, color: Colors.white),
+        ),
+        onPressed: () {},
+      ),
+    );
+  }
+}
