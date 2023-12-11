87a8fddfffd4736c32682c6c96b7acbece54bcf5
Lukasz Walencik <walencik.lukasz@gmail.com>
2023-12-11 16:41:32 +0100

    Inject AuthenticationProvider using MultiProvider and ChangeNotifierProvider (we extends AuthenticationProvider with ChangeNotifier

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:chatify_app/pages/login_page.dart';
 import 'package:chatify_app/pages/splash_page.dart';
+import 'package:chatify_app/providers/authentication_provider.dart';
 import 'package:chatify_app/services/navigation_service.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 main() {
   runApp(MainApp()
@@ -18,17 +20,23 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Chatify',
-      theme: ThemeData(
-        scaffoldBackgroundColor: const Color.fromRGBO(36, 35, 49, 1.0),
-        bottomNavigationBarTheme: const BottomNavigationBarThemeData(
-          backgroundColor: Color.fromRGBO(30, 29, 37, 1.0),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<AuthenticationProvieder>(
+            create: (context) => AuthenticationProvieder())
+      ],
+      child: MaterialApp(
+        title: 'Chatify',
+        theme: ThemeData(
+          scaffoldBackgroundColor: const Color.fromRGBO(36, 35, 49, 1.0),
+          bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+            backgroundColor: Color.fromRGBO(30, 29, 37, 1.0),
+          ),
         ),
+        navigatorKey: NavigationService.navigatorKey,
+        initialRoute: '/login',
+        routes: {'/login': (BuildContext context) => const LoginPage()},
       ),
-      navigatorKey: NavigationService.navigatorKey,
-      initialRoute: '/login',
-      routes: {'/login': (BuildContext context) => const LoginPage()},
     );
   }
 }
