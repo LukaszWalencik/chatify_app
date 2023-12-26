ae6dc3f97985c4f6dd96cd1af1734b2dcabf7327
Lukasz Walencik <walencik.lukasz@gmail.com>
2023-12-26 20:14:53 +0100

    Create simply stateless widget named RoundedImageNetwork with two variables size aqnd path

---
+++ lib/widgets/rounded_imgae.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class RoundedImageNetwork extends StatelessWidget {
+  final String imagePath;
+  final double size;
+  const RoundedImageNetwork(
+      {super.key, required this.imagePath, required this.size});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
