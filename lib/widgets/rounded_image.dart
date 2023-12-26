51756eb916a780355da09ff000e030487095f487
Lukasz Walencik <walencik.lukasz@gmail.com>
2023-12-26 20:58:17 +0100

    add RoundedImageFile to pick image from device

--- lib/widgets/rounded_image.dart
+++ lib/widgets/rounded_image.dart
@@ -1,3 +1,4 @@
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 
 class RoundedImageNetwork extends StatelessWidget {
@@ -20,6 +21,30 @@ class RoundedImageNetwork extends StatelessWidget {
             Radius.circular(size),
           ),
           color: Colors.black),
+    );
+  }
+}
+
+class RoundedImageFile extends StatelessWidget {
+  final PlatformFile image;
+  final double size;
+  const RoundedImageFile({super.key, required this.image, required this.size});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: size,
+      width: size,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          fit: BoxFit.cover,
+          image: AssetImage(image.path!),
+        ),
+        borderRadius: BorderRadius.all(
+          Radius.circular(size),
+        ),
+        color: Colors.black,
+      ),
     );
   }
 }
