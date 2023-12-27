f1d959d5911f310ffd57bc32aae067538f6f74c7
Lukasz Walencik <walencik.lukasz@gmail.com>
2023-12-27 21:06:02 +0100

    Create saveUserImageToStorage function

--- lib/services/cloud_storage_service.dart
+++ lib/services/cloud_storage_service.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 
 // ignore: constant_identifier_names
@@ -5,4 +8,15 @@ const String USER_COLLECTION = 'Users';
 
 class CloudStorageService {
   final FirebaseStorage storage = FirebaseStorage.instance;
+
+  Future<String?> saveUserImageToStorage(String uid, PlatformFile file) async {
+    try {
+      Reference reference =
+          storage.ref().child('images/users/$uid/profile.${file.extension}');
+      UploadTask uploadTask = reference.putFile(File(file.path!));
+      return await uploadTask.then((result) => result.ref.getDownloadURL());
+    } catch (e) {
+      print(e);
+    }
+  }
 }
