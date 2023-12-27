319e1c47c39a6c29c979fffd93e8e76d2b131959
Lukasz Walencik <walencik.lukasz@gmail.com>
2023-12-27 21:35:56 +0100

    Create saveChatImageToStorage function

--- lib/services/cloud_storage_service.dart
+++ lib/services/cloud_storage_service.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 
@@ -15,6 +16,20 @@ class CloudStorageService {
           storage.ref().child('images/users/$uid/profile.${file.extension}');
       UploadTask uploadTask = reference.putFile(File(file.path!));
       return await uploadTask.then((result) => result.ref.getDownloadURL());
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future<String?> saveChatImageToStorage(
+      String chatID, String userID, PlatformFile file) async {
+    try {
+      Reference reference = storage.ref().child(
+          'images/chats/$chatID/${userID}_${Timestamp.now().millisecondsSinceEpoch}.${file.extension}');
+      UploadTask uploadTask = reference.putFile(
+        File(file.path!),
+      );
+      return uploadTask.then((result) => result.ref.getDownloadURL());
     } catch (e) {
       print(e);
     }
