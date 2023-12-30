a9e12b2f2af0cb81733a05570ea92938c6e8edff
Lukasz Walencik <walencik.lukasz@gmail.com>
2023-12-30 23:59:47 +0100

    implement simble bottom navigation bar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,12 +8,41 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int currentPage = 0;
+  List<Widget> pages = [
+    Container(
+      color: Colors.red,
+    ),
+    Container(
+      color: Colors.green,
+    )
+  ];
   @override
   Widget build(BuildContext context) {
     return buildUI();
   }
 
   Widget buildUI() {
-    return Scaffold();
+    return Scaffold(
+      body: pages[currentPage],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentPage,
+        onTap: (value) {
+          setState(() {
+            currentPage = value;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            label: 'Chats',
+            icon: Icon(Icons.bubble_chart),
+          ),
+          BottomNavigationBarItem(
+            label: 'Users',
+            icon: Icon(Icons.supervised_user_circle_sharp),
+          )
+        ],
+      ),
+    );
   }
 }
