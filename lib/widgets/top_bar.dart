23a9032e70271b0e329fe931e7d46e54bab4df1a
Lukasz Walencik <walencik.lukasz@gmail.com>
2024-01-04 13:20:42 +0100

    Change statefull widget to stateless (mistake) add two late viariables of device sizes and write both in build function

--- lib/widgets/top_bar.dart
+++ lib/widgets/top_bar.dart
@@ -1,11 +1,14 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first, must_be_immutable
 import 'package:flutter/material.dart';
 
-class TopBar extends StatefulWidget {
+class TopBar extends StatelessWidget {
   String barTitle;
   Widget? primaryAction;
   Widget? secondaryAction;
   double? fontSize;
+  late double deviceHeight;
+  late double deviceWidth;
+
   TopBar({
     Key? key,
     required this.barTitle,
@@ -15,12 +18,9 @@ class TopBar extends StatefulWidget {
   }) : super(key: key);
 
   @override
-  State<TopBar> createState() => _TopBarState();
-}
-
-class _TopBarState extends State<TopBar> {
-  @override
   Widget build(BuildContext context) {
+    deviceHeight = MediaQuery.of(context).size.height;
+    deviceWidth = MediaQuery.of(context).size.width;
     return buildUI();
   }
 
