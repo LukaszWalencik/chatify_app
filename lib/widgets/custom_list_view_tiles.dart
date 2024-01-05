f998095ccdb7862bd3837095320c60673abae4fc
Lukasz Walencik <walencik.lukasz@gmail.com>
2024-01-05 19:09:59 +0100

    Create CustomListViewTileWithActivity model

---
+++ lib/widgets/custom_list_view_tiles.dart
@@ -0,0 +1,19 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class CustomListViewTileWithActivity {
+  final double height;
+  final String title;
+  final String subtitle;
+  final String image;
+  final bool isActive;
+  final bool isActivity;
+  final Function onTap;
+  CustomListViewTileWithActivity({
+    required this.height,
+    required this.title,
+    required this.subtitle,
+    required this.image,
+    required this.isActive,
+    required this.isActivity,
+    required this.onTap,
+  });
+}
