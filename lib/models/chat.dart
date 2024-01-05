0d91ab99de0f1bd5dae6dc1abb6084c0fb66eb35
Lukasz Walencik <walencik.lukasz@gmail.com>
2024-01-05 18:54:38 +0100

    Create Chat model

---
+++ lib/models/chat.dart
@@ -0,0 +1,24 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:chatify_app/models/chat_message.dart';
+import 'package:chatify_app/models/chat_user.dart';
+
+class Chat {
+  final String uid;
+  final String currentUserUid;
+  final bool activity;
+  final bool group;
+  final List<ChatUser> members;
+  List<ChatMessage> messages;
+  late final List<ChatUser> recepiens;
+  Chat({
+    required this.uid,
+    required this.currentUserUid,
+    required this.activity,
+    required this.group,
+    required this.members,
+    required this.messages,
+  }) {
+    recepiens =
+        members.where((member) => member.uid != currentUserUid).toList();
+  }
+}
