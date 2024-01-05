3acc09624402883f1782273cdc7f560715b0266f
Lukasz Walencik <walencik.lukasz@gmail.com>
2024-01-05 10:58:48 +0100

    Create ChatMessage model

--- lib/models/chat_message.dart
+++ lib/models/chat_message.dart
@@ -5,3 +5,16 @@ enum MessageType {
   IMAGE,
   UNKNOWN,
 }
+
+class ChatMessage {
+  final String senderID;
+  final MessageType type;
+  final String content;
+  final DateTime sentTime;
+  ChatMessage({
+    required this.senderID,
+    required this.type,
+    required this.content,
+    required this.sentTime,
+  });
+}
