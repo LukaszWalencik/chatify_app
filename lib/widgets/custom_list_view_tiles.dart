3c297688154f808ea945a3983b63ce43fe9abc09
Lukasz Walencik <walencik.lukasz@gmail.com>
2024-01-06 16:39:15 +0100

    Add new created widget (RoundedImageNetworkWithStatus) as leading in our ListTile

--- lib/widgets/custom_list_view_tiles.dart
+++ lib/widgets/custom_list_view_tiles.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 
+import 'package:chatify_app/widgets/rounded_image.dart';
 import 'package:flutter/material.dart';
 
 class CustomListViewTileWithActivity extends StatelessWidget {
@@ -26,6 +27,8 @@ class CustomListViewTileWithActivity extends StatelessWidget {
     return ListTile(
       onTap: () => onTap,
       minVerticalPadding: height * 0.20,
+      leading: RoundedImageNetworkWithStatusIndicator(
+          isActive: isActive, imagePath: image, size: height / 2),
       title: Text(
         title,
         style: TextStyle(
