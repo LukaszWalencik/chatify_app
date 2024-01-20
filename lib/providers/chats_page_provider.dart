a7d35d0594e6e70659cd973b3f1c6a9a102e44b0
Lukasz Walencik <walencik.lukasz@gmail.com>
2024-01-20 15:10:52 +0100

    Create Last message for chat

--- lib/providers/chats_page_provider.dart
+++ lib/providers/chats_page_provider.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'dart:async';
 
+import 'package:chatify_app/models/chat_message.dart';
 import 'package:chatify_app/models/chat_user.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
@@ -46,6 +47,16 @@ class ChatsPageProvider extends ChangeNotifier {
                   Map<String, dynamic> userData =
                       userSnapshot.data() as Map<String, dynamic>;
                   members.add(ChatUser.fromJSON(userData));
+                }
+                //Get Last Message For Chat
+                List<ChatMessage> messages = [];
+                QuerySnapshot chatMessage =
+                    await databaseService.getLastMessageForChat(doc.id);
+                if (chatMessage.docs.isNotEmpty) {
+                  Map<String, dynamic> messageData =
+                      chatMessage.docs.first.data()! as Map<String, dynamic>;
+                  ChatMessage message = ChatMessage.fromJSON(messageData);
+                  messages.add(message);
                 }
 
                 //Return Chat Instance
