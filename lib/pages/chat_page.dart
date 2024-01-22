ef73beb287e67a4fba58a7ae97701b0ecda1483e
Lukasz Walencik <walencik.lukasz@gmail.com>
2024-01-22 22:10:43 +0100

    Add variables to ChatPage

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,3 +1,4 @@
+import 'package:chatify_app/providers/authentication_provider.dart';
 import 'package:flutter/material.dart';
 
 class ChatPage extends StatefulWidget {
@@ -8,6 +9,11 @@ class ChatPage extends StatefulWidget {
 }
 
 class _ChatPageState extends State<ChatPage> {
+  late double deviceHeight;
+  late double deviceWeight;
+  late AuthenticationProvieder authenticationProvieder;
+  late GlobalKey<FormState> messageFormState;
+  late ScrollController messageListViewController;
   @override
   Widget build(BuildContext context) {
     return Container();
