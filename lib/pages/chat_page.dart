1abfb6997a889d89d945f07fab7b309f697fe069
Lukasz Walencik <walencik.lukasz@gmail.com>
2024-01-22 22:31:54 +0100

    Write simple body of ChatPage

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,5 +1,6 @@
 import 'package:chatify_app/models/chat.dart';
 import 'package:chatify_app/providers/authentication_provider.dart';
+import 'package:chatify_app/widgets/top_bar.dart';
 import 'package:flutter/material.dart';
 
 class ChatPage extends StatefulWidget {
@@ -12,16 +13,44 @@ class ChatPage extends StatefulWidget {
 
 class _ChatPageState extends State<ChatPage> {
   late double deviceHeight;
-  late double deviceWeight;
+  late double deviceWidth;
   late AuthenticationProvieder authenticationProvieder;
   late GlobalKey<FormState> messageFormState;
   late ScrollController messageListViewController;
   @override
   Widget build(BuildContext context) {
+    deviceHeight = MediaQuery.of(context).size.height;
+    deviceWidth = MediaQuery.of(context).size.width;
     return buildUI();
   }
 
   Widget buildUI() {
-    return Scaffold();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.symmetric(
+            horizontal: deviceWidth * 0.03,
+            vertical: deviceHeight * 0.02,
+          ),
+          height: deviceHeight * 0.98,
+          width: deviceWidth * 0.97,
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              TopBar(
+                barTitle: this.widget.chat.title(),
+                primaryAction: IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.delete),
+                  color: Color.fromRGBO(0, 82, 218, 1.0),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
