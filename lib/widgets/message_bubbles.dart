8c7a3b534ee6d8ca346ee54ea89bfed56526c0b5
Lukasz Walencik <walencik.lukasz@gmail.com>
2024-01-26 23:14:27 +0100

    upgrade TextMessageBubble

--- lib/widgets/message_bubbles.dart
+++ lib/widgets/message_bubbles.dart
@@ -1,5 +1,6 @@
 import 'package:chatify_app/models/chat_message.dart';
 import 'package:flutter/material.dart';
+import 'package:timeago/timeago.dart' as timeago;
 
 class TextMessageBubble extends StatelessWidget {
   final bool isOwnMessage;
@@ -29,12 +30,32 @@ class TextMessageBubble extends StatelessWidget {
       width: width,
       padding: const EdgeInsets.symmetric(horizontal: 10),
       decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          gradient: LinearGradient(
-              colors: colorSheme,
-              stops: const [0.30, 0.70],
-              begin: Alignment.bottomLeft,
-              end: Alignment.centerRight)),
+        borderRadius: BorderRadius.circular(10),
+        gradient: LinearGradient(
+            colors: colorSheme,
+            stops: const [0.30, 0.70],
+            begin: Alignment.bottomLeft,
+            end: Alignment.centerRight),
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            message.content,
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          Text(
+            timeago.format(message.sentTime),
+            style: TextStyle(
+              color: Colors.white70,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
