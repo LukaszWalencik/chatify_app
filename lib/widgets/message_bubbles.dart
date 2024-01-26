8a54782c7bebf297a853c6d469e7a393831fb312
Lukasz Walencik <walencik.lukasz@gmail.com>
2024-01-26 22:41:47 +0100

    Create TextMessageBubble

---
+++ lib/widgets/message_bubbles.dart
@@ -0,0 +1,40 @@
+import 'package:chatify_app/models/chat_message.dart';
+import 'package:flutter/material.dart';
+
+class TextMessageBubble extends StatelessWidget {
+  final bool isOwnMessage;
+  final ChatMessage message;
+  final double width;
+  final double height;
+  const TextMessageBubble(
+      {super.key,
+      required this.isOwnMessage,
+      required this.message,
+      required this.width,
+      required this.height});
+
+  @override
+  Widget build(BuildContext context) {
+    List<Color> colorSheme = isOwnMessage
+        ? [
+            const Color.fromRGBO(0, 136, 249, 1),
+            const Color.fromRGBO(0, 82, 218, 1)
+          ]
+        : [
+            const Color.fromRGBO(51, 49, 68, 1),
+            const Color.fromRGBO(51, 49, 68, 1)
+          ];
+    return Container(
+      height: height + (message.content.length / 20 * 6.0),
+      width: width,
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          gradient: LinearGradient(
+              colors: colorSheme,
+              stops: const [0.30, 0.70],
+              begin: Alignment.bottomLeft,
+              end: Alignment.centerRight)),
+    );
+  }
+}
