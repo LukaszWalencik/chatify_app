d6f85662d6a2c27c08504a26c97f4ab1bf67a607
Lukasz Walencik <walencik.lukasz@gmail.com>
2024-01-28 16:10:29 +0100

    Create ImageMessageBubble widget

--- lib/widgets/message_bubbles.dart
+++ lib/widgets/message_bubbles.dart
@@ -34,7 +34,7 @@ class TextMessageBubble extends StatelessWidget {
         gradient: LinearGradient(
             colors: colorSheme,
             stops: const [0.30, 0.70],
-            begin: Alignment.bottomLeft,
+            begin: Alignment.centerLeft,
             end: Alignment.centerRight),
       ),
       child: Column(
@@ -44,13 +44,77 @@ class TextMessageBubble extends StatelessWidget {
         children: [
           Text(
             message.content,
-            style: TextStyle(
+            style: const TextStyle(
               color: Colors.white,
             ),
           ),
           Text(
             timeago.format(message.sentTime),
-            style: TextStyle(
+            style: const TextStyle(
+              color: Colors.white70,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ImageMessageBubble extends StatelessWidget {
+  final bool isOwnMessage;
+  final ChatMessage chatMessage;
+  final double height;
+  final double width;
+
+  const ImageMessageBubble(
+      {super.key,
+      required this.isOwnMessage,
+      required this.chatMessage,
+      required this.height,
+      required this.width});
+
+  @override
+  Widget build(BuildContext context) {
+    List<Color> colorSheme = isOwnMessage
+        ? [
+            const Color.fromRGBO(0, 136, 249, 1),
+            const Color.fromRGBO(0, 82, 218, 1)
+          ]
+        : [
+            const Color.fromRGBO(51, 49, 68, 1),
+            const Color.fromRGBO(51, 49, 68, 1)
+          ];
+    DecorationImage decorationImage = DecorationImage(
+        image: NetworkImage(chatMessage.content), fit: BoxFit.cover);
+    return Container(
+      padding: EdgeInsets.symmetric(
+        horizontal: width * 0.02,
+        vertical: height * 0.03,
+      ),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        gradient: LinearGradient(
+            colors: colorSheme,
+            stops: const [0.30, 0.70],
+            begin: Alignment.bottomLeft,
+            end: Alignment.bottomRight),
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            height: height,
+            width: width,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                image: decorationImage),
+          ),
+          SizedBox(height: height * 0.02),
+          Text(
+            timeago.format(chatMessage.sentTime),
+            style: const TextStyle(
               color: Colors.white70,
             ),
           ),
