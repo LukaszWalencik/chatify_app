d309835b672d5f43bdf73be557ce6fd1b905b4cf
Lukasz Walencik <walencik.lukasz@gmail.com>
2024-01-31 23:17:09 +0100

    add late values and write it in build method

--- lib/pages/user_page.dart
+++ lib/pages/user_page.dart
@@ -1,4 +1,6 @@
+import 'package:chatify_app/providers/authentication_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class UserPage extends StatefulWidget {
   const UserPage({super.key});
@@ -8,8 +10,17 @@ class UserPage extends StatefulWidget {
 }
 
 class _UserPageState extends State<UserPage> {
+  late double deviceHeight;
+  late double deviceWidth;
+
+  late AuthenticationProvieder authenticationProvieder;
+  final TextEditingController searchFieldEditingcontroller =
+      TextEditingController();
   @override
   Widget build(BuildContext context) {
+    deviceHeight = MediaQuery.of(context).size.height;
+    deviceWidth = MediaQuery.of(context).size.width;
+    authenticationProvieder = Provider.of<AuthenticationProvieder>(context);
     return buildUI();
   }
 
