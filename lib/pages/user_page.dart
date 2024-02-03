97ff453286d8dd026ff979f0651155a14ffe5f48
Lukasz Walencik <walencik.lukasz@gmail.com>
2024-02-03 15:59:27 +0100

    Wrap buildUI body with Builder widget to remove failure of builder

--- lib/pages/user_page.dart
+++ lib/pages/user_page.dart
@@ -37,36 +37,38 @@ class _UserPageState extends State<UserPage> {
   }
 
   Widget buildUI() {
-    return Container(
-      padding: EdgeInsets.symmetric(
-        vertical: deviceWidth * 0.03,
-        horizontal: deviceHeight * 0.02,
-      ),
-      height: deviceHeight * 0.98,
-      width: deviceWidth * 0.97,
-      child: Column(
-        mainAxisSize: MainAxisSize.max,
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          TopBar(
-            barTitle: 'Users',
-            primaryAction: IconButton(
-                onPressed: () {},
-                icon: Icon(
-                  Icons.logout,
-                  color: Color.fromRGBO(0, 82, 218, 1),
-                )),
-          ),
-          CustomTextField(
-              onEditingComplete: (value) {},
-              hintText: 'Search..',
-              obscureText: false,
-              textEditingController: searchFieldEditingcontroller),
-          usersList(),
-        ],
-      ),
-    );
+    return Builder(builder: (context) {
+      return Container(
+        padding: EdgeInsets.symmetric(
+          vertical: deviceWidth * 0.03,
+          horizontal: deviceHeight * 0.02,
+        ),
+        height: deviceHeight * 0.98,
+        width: deviceWidth * 0.97,
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            TopBar(
+              barTitle: 'Users',
+              primaryAction: IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.logout,
+                    color: Color.fromRGBO(0, 82, 218, 1),
+                  )),
+            ),
+            CustomTextField(
+                onEditingComplete: (value) {},
+                hintText: 'Search..',
+                obscureText: false,
+                textEditingController: searchFieldEditingcontroller),
+            usersList(),
+          ],
+        ),
+      );
+    });
   }
 
   Widget usersList() {
